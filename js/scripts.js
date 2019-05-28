a649e5428efc9d87dd0a98ed22e7d6ffaa16ac72
YzakV <isaac.martinez@lakmisystems.com.mx>
2019-05-28 18:38:01 -0500

    Update

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$("#boton-servicios").click(function(){
+   $("#contenedor-servicios-1").toggle();
+   $("#contenedor-servicios-2").toggle();
+});
+
+$("#boton-fintech").click(function(){
+   $("#contenedor-servicios-1").toggle();
+   $("#contenedor-servicios-2").toggle();
+});
